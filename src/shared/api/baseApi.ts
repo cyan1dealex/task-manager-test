c03f0bb462811426a37f49285ae471876ece2468
cyan1dealex <cyan1de.wot@mail.ru>
2026-08-20 14:34:19 +0300

    feat: switch api to mockapi and add env configuration

--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -1,8 +1,5 @@
 import axios from 'axios'
 
 export const baseApi = axios.create({
-	baseURL: 'http://localhost:3001',
-	headers: {
-		'Content-Type': 'application/json',
-	},
+	baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
 })
